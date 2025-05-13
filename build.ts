6d05d170f456f4e7b5f2a7947f0032aec3a61079
ruslan.gulid <ruslan.gulid@gmail.com>
2025-05-13 13:34:46 +0000

    an example + removed cjs building as bun does not support it

--- build.ts
+++ build.ts
@@ -1,21 +1,21 @@
-import type { BuildConfig } from 'bun'
-import dts from 'bun-plugin-dts'
+import type { BuildConfig } from "bun";
+import dts from "bun-plugin-dts";
 
 const defaultBuildConfig: BuildConfig = {
-  entrypoints: ['./src/index.ts'],
-  outdir: './dist'
-}
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+};
 
 await Promise.all([
   Bun.build({
     ...defaultBuildConfig,
     plugins: [dts()],
-    format: 'esm',
+    format: "esm",
     naming: "[dir]/[name].js",
   }),
   Bun.build({
     ...defaultBuildConfig,
-    format: 'cjs',
-    naming: "[dir]/[name].cjs",
-  })
-])
+    format: "esm",
+    naming: "[dir]/[name].js",
+  }),
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./banner";
 export * from "./pattern";
 export * from "./dye-color";
+export * from "./texture-renderer"
